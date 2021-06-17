465ccad0e55404ff2fba04e41cb8a78a737813b5
intanpujazaeni <intanpujazaeni@gmail.com>
2021-06-17 11:49:25 +0700

    Create Database dan Show Data

---
+++ connectormysqlneatbeans/Project2-IntanPujaZaeni-XIRPL1/src/tbl_siswa.java
@@ -0,0 +1,20 @@
+
+import javax.swing.table.DefaultTableModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author K_707
+ */
+class tbl_siswa {
+
+    static void setMode(DefaultTableModel dtm) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
